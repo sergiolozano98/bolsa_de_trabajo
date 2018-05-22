6cae557d75e5840106278a3494e05d3eb04ed424
TxusTxus <39275216+TxusTxus@users.noreply.github.com>
2018-05-22 15:38:31 +0200

    Acción nuevo usuario desde empresa rol admin

--- src/UsuariosBundle/Form/UserType.php
+++ src/UsuariosBundle/Form/UserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -20,16 +21,17 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email', EmailType::class, array('label' => 'Correo electrónico') )
-            ->add('username', TextType::class, array('label' => 'Usuario'))
+            ->add('email', EmailType::class, array('label' => 'Correo electrónico'))
             ->add('nombre', TextType::class, array('label' => 'Nombre'))
             ->add('nif', TextType::class, array('label' => 'NIF'))
-            ->add('username', TextType::class)
-             ->add('plainPassword', RepeatedType::class, array(
-                'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Contraseña'),
-                'second_options' => array('label' => 'Repita contraseña')))
-             ->add('Insert',SubmitType::class);
+            ->add('plainPassword', RepeatedType::class, array(
+                'type'              => PasswordType::class,
+                'first_options'     => array('label' => 'Contraseña'),
+                'second_options'    => array('label' => 'Repita contraseña')))
+             ->add('Roles', ChoiceType::class, array('choices'  => array(
+                            'Editor'   => true,
+                            'Usuario'   => true,
+                            'Admin'    => true)));
     }
 
     /**
